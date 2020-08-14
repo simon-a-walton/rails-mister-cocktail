2a642a7da94c0737e8938c0380e677ed35e43268
Simon Walton <saw29@hotmail.co.uk>
2020-08-14 15:28:43 +0100

    to heruku

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -4,29 +4,29 @@
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
 
-// import $ from 'jquery';
-// import 'select2';
+// // import $ from 'jquery';
+// // import 'select2';
 
-// $('#city-input').select2();
+// // $('#city-input').select2();
 
-const results = document.querySelector('#cocktail-image');
+// const results = document.querySelector('#cocktail-image');
 
-const searchImage = (imageName) => {
-  const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${imageName}`;
-  fetch(url)
-    .then(response => response.json())
-    .then((data) => {
-      const imageIcon = `<img src=http://openweathermap.org/img/w/${data.drinks[0].strDrinkThumb}.png>`;
-      results.insertAdjacentHTML('beforeend', imageIcon);
-    });
-};
+// const searchImage = (imageName) => {
+//   const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${imageName}`;
+//   fetch(url)
+//     .then(response => response.json())
+//     .then((data) => {
+//       const imageIcon = `<img src=http://openweathermap.org/img/w/${data.drinks[0].strDrinkThumb}.png>`;
+//       results.insertAdjacentHTML('beforeend', imageIcon);
+//     });
+// };
 
-const input = document.querySelector(".submit");
-input.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const imageName = document.getElementsByClassName('input').value;
-  results.innerHTML = '';
-  searchLocation(imageName);
-});
+// const input = document.querySelector(".submit");
+// input.addEventListener('submit', (event) => {
+//   event.preventDefault();
+//   const imageName = document.getElementsByClassName('input').value;
+//   results.innerHTML = '';
+//   searchLocation(imageName);
+// });
 
-export { searchImage }
+// export { searchImage  }
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,23 +8,23 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-import { searchImage } from './channels/index.js'
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
-import 'bootstrap';
+// import { searchImage } from './channels/index.js'
+// // Uncomment to copy all static images under ../images to the output folder and reference
+// // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// // or the `imagePath` JavaScript helper below.
+// //
+// // const images = require.context('../images', true)
+// // const imagePath = (name) => images(name, true)
+// import 'bootstrap';
 
-// import $ from 'jquery';
-// import 'select2';
+// // import $ from 'jquery';
+// // import 'select2';
 
-// $('#city-input').select2();
+// // $('#city-input').select2();
 
-// const ingredients = ["Lemon","Bali","Barcelona","Belo Horizonte","Berlin","Bordeaux","Brussels","Buenos Aires","Casablanca","Chengdu","Copenhagen","Kyoto","Lausanne","Lille","Lisbon","London","Lyon","Madrid","Marseille","Melbourne","Mexico","Milan","Montréal","Nantes","Oslo","Paris","Rio de Janeiro","Rennes","Rome","São Paulo","Seoul","Shanghai","Shenzhen","Singapore","Stockholm","Tel Aviv","Tokyo"];
+// // const ingredients = ["Lemon","Bali","Barcelona","Belo Horizonte","Berlin","Bordeaux","Brussels","Buenos Aires","Casablanca","Chengdu","Copenhagen","Kyoto","Lausanne","Lille","Lisbon","London","Lyon","Madrid","Marseille","Melbourne","Mexico","Milan","Montréal","Nantes","Oslo","Paris","Rio de Janeiro","Rennes","Rome","São Paulo","Seoul","Shanghai","Shenzhen","Singapore","Stockholm","Tel Aviv","Tokyo"];
 
-// $('#city-input').select2({ data: cities, width: '100%' });
-document.addEventListener('turbolinks:load', () => {;
-  searchImage();
-});
+// // $('#city-input').select2({ data: cities, width: '100%' });
+// document.addEventListener('turbolinks:load', () => {;
+//   searchImage();
+// });
